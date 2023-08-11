610992e925334c13128563fcb1040035a51de543
Robert.minia <omid-bakeri>
2023-08-11 15:48:34 +0330

    file updates --- response

--- api/v1/Cities/index.php
+++ api/v1/Cities/index.php
@@ -6,7 +6,7 @@ use App\Utilities\Response;
 include '../../../autoloader.php';
 
 $cs = new CityServices();
-$result  = $cs->getCities([1 ,2 , 3]);
+$result  = $cs->getCities();
 
 $re = new Response();
 echo Response::ResponseData($result , 200);
